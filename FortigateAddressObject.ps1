113ef44446aaad35743759e0d1fd13a15031d57c
wizzard-lizzard <45213416+wizzard-lizzard@users.noreply.github.com>
2021-07-06 11:19:38 -0400

    Fixed an issue with the Group set member command

--- FortigateAddressObject.ps1
+++ FortigateAddressObject.ps1
@@ -1,4 +1,4 @@
-﻿param (
+param (
     [Parameter(Mandatory)]
     [string]$InFile,
     [Parameter(Mandatory)]
@@ -13,10 +13,11 @@ $regex = [Regex]::new('(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})(/\d{2})')
 
 echo "config firewall address" | Set-Content -Path $OutFile
 echo $infile
+$GroupSetCommand = "set member "
 foreach($address in [System.IO.File]::ReadLines($InFile)) {
         $ip_only = $address -replace $regex, '$1.$2.$3.$4'
         $address_command = "edit $Prefix-$ip_only"
-        $GroupSetCommand = $GroupSetCommand + "`"$Prefix-$address`" "
+        $GroupSetCommand = $GroupSetCommand + "`"$Prefix-$ip_only`" "
         echo $address_command | Add-Content -Path $OutFile
         echo "set subnet $address" | Add-Content -Path $OutFile
         echo "next" | Add-Content -Path $OutFile
